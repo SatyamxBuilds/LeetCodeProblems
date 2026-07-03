e5abc716cfbc851c5b361fda6f953690f5bc2e9c
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-03 22:26:25 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.72%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,9 @@
+__attribute__((always_inline))
+int mySqrt(int x) {
+    if (x == 0) return 0;
+    long r = x;
+    while (r * r > x) {
+        r = (r + x / r) >> 1; 
+    }
+    return (int)r;
+}
