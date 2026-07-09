cedd3c3a21605df06487af3fbfe015818099b1df
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-10 00:07:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (95.32%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,3 +1,18 @@
 int strStr(char* haystack, char* needle) {
-    
+    int m = 0;
+    while (haystack[m] != '\0') m++;
+    int n = 0;
+    while (needle[n] != '\0') n++;
+    if (n == 0) return 0;
+    if (m < n) return -1;
+    for (int i = 0; i <= m - n; i++) {
+        int j = 0;
+        while (j < n && haystack[i + j] == needle[j]) {
+            j++;
+        }
+        if (j == n) {
+            return i;
+        }
+    }
+    return -1;
 }
