f93da6521f95b5ec41d91d31a0e6e744b9012d1f
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-10 00:14:23 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.34%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -1,15 +1,11 @@
-#include <stdbool.h>
-
 bool isPalindrome(int x) {
-
     if (x < 0 || (x % 10 == 0 && x != 0)) {
         return false;
     }
-
-    int reversedNumber = 0;
-    while (x > reversedNumber) {
-        reversedNumber = reversedNumber * 10 + x % 10;
+    int reversedHalf = 0;
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + x % 10;
         x /= 10;
     }
-    return x == reversedNumber || x == reversedNumber / 10;
+    return x == reversedHalf || x == reversedHalf / 10;
 }
