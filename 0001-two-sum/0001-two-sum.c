79acc1945008084bfa38b8cee7e54c34c7e12ec1
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-09 23:19:01 +0530

    Time: 104 ms (20.9%), Space: 9.4 MB (73.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,33 +1,15 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int capacity = numsSize * 2;
-    if (capacity < 8) capacity = 8;
-    int* keys = (int*)malloc(capacity * sizeof(int));
-    int* indices = (int*)malloc(capacity * sizeof(int));
-    for (int i = 0; i < capacity; i++) indices[i] = -1;
     int* result = (int*)malloc(2 * sizeof(int));
     *returnSize = 2;
     for (int i = 0; i < numsSize; i++) {
-        int complement = target - nums[i];
-        unsigned int h = (unsigned int)complement % capacity;
-        while (indices[h] != -1) {
-            if (keys[h] == complement) {
-                result[0] = indices[h];
-                result[1] = i;
-                free(keys);
-                free(indices);
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
                 return result;
             }
-            h = (h + 1) % capacity;
         }
-        unsigned int hi = (unsigned int)nums[i] % capacity;
-        while (indices[hi] != -1) {
-            hi = (hi + 1) % capacity;
-        }
-        keys[hi] = nums[i];
-        indices[hi] = i;
     }
-    free(keys);
-    free(indices);
     *returnSize = 0;
     return NULL;
 }
