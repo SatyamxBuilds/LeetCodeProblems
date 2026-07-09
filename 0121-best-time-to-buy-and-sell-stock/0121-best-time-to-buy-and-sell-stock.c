02b57ec9eb66357841806bd216182c7f6ff4e8b4
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-10 00:16:09 +0530

    Time: 4 ms (16.38%), Space: 16 MB (87.28%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.c
@@ -1,26 +1,16 @@
-int findMin(int a, int b)
-{
-    return (a < b) ? a : b;
-}
-
-int findMax(int a, int b)
-{
-    return (a > b) ? a : b;
-}
-
-int maxProfit(int* prices, int pricesSize)
-{
-    if(pricesSize == 0)
-        return 0;
-
+int maxProfit(int* prices, int pricesSize) {
+    if (pricesSize <= 1) return 0;
     int minPrice = prices[0];
     int maxProfit = 0;
-
-    for(int i = 1; i < pricesSize; i++)
-    {
-        minPrice = findMin(minPrice, prices[i]);
-        maxProfit = findMax(maxProfit, prices[i] - minPrice);
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            int currentProfit = prices[i] - minPrice;
+            if (currentProfit > maxProfit) {
+                maxProfit = currentProfit;
+            }
+        }
     }
-
     return maxProfit;
 }
