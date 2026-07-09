364ad0afb7798c8c0e5eb3a39012f31d5a9a97df
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-10 00:23:21 +0530

    Time: 0 ms (100%), Space: 8.9 MB (99.24%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.c
+++ 0029-divide-two-integers/0029-divide-two-integers.c
@@ -1,20 +1,21 @@
 #pragma GCC optimize("O3")
+
 int divide(int dividend, int divisor) {
     if (dividend == INT_MIN && divisor == -1) return INT_MAX;
     if (dividend == INT_MIN && divisor == 1) return INT_MIN;
+
     long long absDividend = labs(dividend);
     long long absDivisor = labs(divisor);
-    int quotient = 0;
+    long long quotient = 0;
+
     int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
-    while (absDividend >= absDivisor) {
-        long long tempDivisor = absDivisor;
-        long long multiple = 1;
-        while (absDividend >= (tempDivisor << 1)) {
-            tempDivisor <<= 1;
-            multiple <<= 1;
+
+    for (int i = 31; i >= 0; i--) {
+        if ((absDivisor << i) <= absDividend) {
+            absDividend -= (absDivisor << i);
+            quotient |= (1LL << i);
         }
-        absDividend -= tempDivisor;
-        quotient += multiple;
     }
+
     return sign * quotient;
 }
