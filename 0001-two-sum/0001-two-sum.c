b839df874f13afe936c3d24c70ad049df8e9f134
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-09 23:24:02 +0530

    Time: 101 ms (48.37%), Space: 9.5 MB (42.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,45 +1,15 @@
-typedef struct Node
-{
-    int key;
-    int index;
-    struct Node *next;
-} Node;
-
-int* twoSum(int* nums, int numsSize, int target, int* returnSize)
-{
-    const int HASH_SIZE = 16384;      // Power of 2
-    Node* table[16384] = {0};
-
-    for(int i = 0; i < numsSize; i++)
-    {
-        int complement = target - nums[i];
-
-        unsigned int hash = ((unsigned int)complement) & (HASH_SIZE - 1);
-
-        Node *cur = table[hash];
-
-        while(cur)
-        {
-            if(cur->key == complement)
-            {
-                int *ans = (int*)malloc(sizeof(int) * 2);
-                ans[0] = cur->index;
-                ans[1] = i;
-                *returnSize = 2;
-                return ans;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    *returnSize = 2;
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
             }
-            cur = cur->next;
         }
-
-        hash = ((unsigned int)nums[i]) & (HASH_SIZE - 1);
-
-        Node *node = (Node*)malloc(sizeof(Node));
-        node->key = nums[i];
-        node->index = i;
-        node->next = table[hash];
-        table[hash] = node;
     }
-
     *returnSize = 0;
     return NULL;
 }
