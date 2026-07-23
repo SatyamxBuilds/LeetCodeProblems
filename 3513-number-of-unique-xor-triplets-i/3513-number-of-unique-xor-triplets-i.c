8f8c31d84ec2e5eb6cfac971538d000f88647260
Satyam Pandey <satyamxbuilds@gmail.com>
2026-07-23 16:05:15 +0530

    Time: 0 ms (100%), Space: 22.4 MB (83.33%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.c
@@ -0,0 +1,11 @@
+#pragma GCC optimize("O3")
+
+int uniqueXorTriplets(int* nums, int numsSize) {
+    if (numsSize < 3) {
+        return numsSize;
+    }
+    
+    int bitLength = 32 - __builtin_clz(numsSize);
+    
+    return 1 << bitLength;
+}
